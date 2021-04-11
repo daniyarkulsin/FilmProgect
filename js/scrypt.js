cfd0b4979639dda4e7e1db362bcffd1770b10811
unknown <daniar.kulsin@gmail.com>
2021-04-11 23:09:29 +0600

    первый

---
+++ js/scrypt.js
@@ -0,0 +1,21 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieaDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieaDB.movies[a] = b;
+personalMovieaDB.movies[c] = d; 
+
+
+
+console.log(personalMovieaDB);
