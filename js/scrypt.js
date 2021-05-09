40688e40bbc9278ccf2cff7e3ee65ab73e670511
unknown <daniar.kulsin@gmail.com>
2021-05-10 01:04:24 +0600

    one

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,24 +1,23 @@
 
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
 
 const personalMovieaDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    generes: [],
+    genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+const a = prompt('Один из просмотренных фильмов?',''),
+      b = prompt ('На сколько оцените его?',''),
+      c = prompt('Один из просмотренных фильмов?',''),
+      d = prompt ('На сколько оцените его?','');
 
 personalMovieaDB.movies[a] = b;
-personalMovieaDB.movies[c] = d; 
+personalMovieaDB.movies[c] = d;
 
-
-
-console.log(personalMovieaDB);
+console.log(personalMovieaDB)
