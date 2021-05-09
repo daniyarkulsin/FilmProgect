335e306fae1762c4658d7d0f7a32a05806773dde
unknown <daniar.kulsin@gmail.com>
2021-05-10 02:13:58 +0600

    two

--- js/scrypt.js
+++ js/scrypt.js
@@ -12,12 +12,29 @@ const personalMovieaDB = {
     privat: false
 };
 
-const a = prompt('Один из просмотренных фильмов?',''),
-      b = prompt ('На сколько оцените его?',''),
-      c = prompt('Один из просмотренных фильмов?',''),
-      d = prompt ('На сколько оцените его?','');
+if (personalMovieaDB.count < 10){
+    console.log('Просмотренно давольно мало фильмов!');
+} else if (personalMovieaDB.count >= 10 && personalMovieaDB.count < 30) {
+    console.log('Вы класический зритель');
+} else if(personalMovieaDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Пройзошла ошибка');
+}
 
-personalMovieaDB.movies[a] = b;
-personalMovieaDB.movies[c] = d;
 
-console.log(personalMovieaDB)
+for (let i = 0; i < 2; i++){
+    let a = prompt('Один из просмотренных фильмов?',''),
+        b = prompt ('На сколько оцените его?','');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieaDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error')
+        i--;
+    }
+
+}
+
+console.log(personalMovieaDB)  
