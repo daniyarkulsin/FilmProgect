5f2aa9524d4a22d707958b43505983c2fad8efdb
unknown <daniar.kulsin@gmail.com>
2021-05-22 16:05:09 +0600

    Fuction

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,40 +1,64 @@
 
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let numberOfFilms;
 
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','')
+    }
+}
+
+start();
 
 const personalMovieaDB = {
-    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-if (personalMovieaDB.count < 10){
-    console.log('Просмотренно давольно мало фильмов!');
-} else if (personalMovieaDB.count >= 10 && personalMovieaDB.count < 30) {
-    console.log('Вы класический зритель');
-} else if(personalMovieaDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Пройзошла ошибка');
-}
 
 
-for (let i = 0; i < 2; i++){
-    let a = prompt('Один из просмотренных фильмов?',''),
-        b = prompt ('На сколько оцените его?','');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieaDB.movies[a] = b;
-        console.log('done');
+function rememberMyFilms() {
+    if (personalMovieaDB.count < 10){
+        console.log('Просмотренно давольно мало фильмов!');
+    } else if (personalMovieaDB.count >= 10 && personalMovieaDB.count < 30) {
+        console.log('Вы класический зритель');
+    } else if(personalMovieaDB.count >= 30) {
+        console.log('Вы киноман');
     } else {
-        console.log('Error')
-        i--;
+        console.log('Пройзошла ошибка');
     }
-
 }
 
-console.log(personalMovieaDB)  
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    for (let i = 0; i < 2; i++){
+        let a = prompt('Один из просмотренных фильмов?',''),
+            b = prompt ('На сколько оцените его?','');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieaDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('Error')
+            i--;
+        }
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieaDB);
+    }
+}
+
+showMyDB(personalMovieaDB.privat);
